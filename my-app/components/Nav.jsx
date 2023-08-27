e3fc0f3a735e543f6f0db1497c48ca25c3d230a3
MALARVIZHI M <Malarm2004@gmail.com>
2023-08-27 14:58:09 +0530

    Starting commit

---
+++ my-app/components/Nav.jsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+import Image from "next/image";
+import { useState, useEffect } from "react";
+import { signIn, signOut, useSession, getProviders } from "next-auth/react";
+
+const Nav = () => {
+  return <div>Nav</div>;
+};
+
+export default Nav;
